178553c10c0e0d62bf89fe20aa392e9413c21409
a-dogapp <bark-genes-4w@icloud.com>
2025-05-04 18:19:24 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,7 @@
-alert("JavaScript is working!");
+function updateScore() {
+    let teamAScore = document.getElementById("teamA-1");
+    let teamBScore = document.getElementById("teamB-1");
+
+    teamAScore.innerText = parseInt(teamAScore.innerText) + 1;  // Increase Team A's score
+    teamBScore.innerText = parseInt(teamBScore.innerText) + 1;  // Increase Team B's score
+}
